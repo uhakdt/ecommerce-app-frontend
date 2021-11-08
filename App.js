154935a74f7c23398e354c9a9cd4099ebe68f018
uhakdt <uhakdt@gmail.com>
2021-11-08 22:55:03 +0000

    I am going to become the King of the Pirates. If that means I will die on the journey, so be it.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'red', paddingHorizontal: 20}}>I am going to become the King of the Pirates. If that means I will die on the journey, so be it.</Text>
       <StatusBar style="auto" />
     </View>
   );
